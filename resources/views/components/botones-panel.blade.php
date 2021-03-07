162f8a233af74f3f77fb62bda29feec2215610ab
isaiasjbp <isaias.balmaceda@gmial.com>
2021-03-07 18:20:38 -0500

    add .env

--- resources/views/components/botones-panel.blade.php
+++ resources/views/components/botones-panel.blade.php
@@ -5,8 +5,8 @@
                                 <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users"></i></span>
                                 <div class="info-box-content">
                                     <span class="info-box-text">Usuarios</span>
-                                    <span class="info-box-number"> 
-                                       {{count($datos['usuarios'])}} 
+                                    <span class="info-box-number">
+                                       {{count($datos['usuarios'])}}
                                     </span>
                                 </div>
                                 <!-- /.info-box-content -->
@@ -35,12 +35,24 @@
 
                         <div class="col-12 col-sm-6 col-md-3 raton" onClick="llamarComponente('get/modelos','visorCompontes');">
                             <div class="info-box mb-3">
-                                <span class="info-box-icon bg-success elevation-1"><i
-                                        class="fas fa-car"></i></span>
-
+                                <span class="info-box-icon bg-success elevation-1"><i class="fas fa-car"></i></span>
                                 <div class="info-box-content">
                                     <span class="info-box-text">Modelos</span>
                                     <span class="info-box-number">{{count($datos['modelos'])}}</span>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
+                        <!-- /.col -->
+                        <div class="clearfix hidden-md-up"></div>
+
+                        <div class="col-12 col-sm-6 col-md-3 raton" onClick="llamarComponente('get/grafica','visorCompontes');">
+                            <div class="info-box mb-3">
+                                <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-chart-line"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Grafica</span>
+
                                 </div>
                                 <!-- /.info-box-content -->
                             </div>
